da3a725e05dceb40cf8827a740580a066990768f
JuanCarlosAlvaradoRucobo <144263791+JuanCarlosAlvaradoRucobo@users.noreply.github.com>
2024-11-08 21:32:11 -0800

    example in ui
    example code gdscript variables

---
+++ UI/Example.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+var score = 5
+var new_score
+
+func add_to_score(points):
+	# new_score is accidentally re-declared
+	# with local scope in this indented code block
+	var new_score = score + points
+
+func _ready():
+	add_to_score(1)
+	# new_score has not been set
+	print(new_score) # prints null
