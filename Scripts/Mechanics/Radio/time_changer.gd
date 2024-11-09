884aaf28a19890d4ab852eaa0ab9e09afe3086a9
Adalberto Gonzalez <drafty343@gmail.com>
2024-11-08 23:47:21 -0800

    Actualizacion de la script de la radio

--- Scripts/Mechanics/Radio/time_changer.gd
+++ Scripts/Mechanics/Radio/time_changer.gd
@@ -3,16 +3,49 @@ extends Node
 @export var radio: Node
 @export var default_scene: Node3D
 @export var alternative_scene: Node3D
-@export var time_freq = 1105
+@export var alt_freq = 1023
+@export var def_freq = 1058
+const max_timer = 1.5
 var timer = 0
+var alternative_time = false
 
 func _ready() -> void:
-	pass
+	alternative_scene.set_process(false)
+	alternative_scene.set_physics_process(false)
+	alternative_scene.set_visible(false)
 
 
 func _physics_process(delta):
-	print("Current freq: %.2f\nTimer: %.2f" % [radio.freq, timer])
-	if radio.freq > time_freq - 1.5 and radio.freq < time_freq + 1.5:
-		if timer < 3.5: timer += 1.0 * delta
+	print("Current freq: %.2f\nTimer: %.2f\nAlt time: %s" % [radio.freq, timer, alternative_time])
+	#Manejo del timer, si se mantiene la frecuencia el timer incrementa, caso contrario decrese
+	if alternative_time:
+		if radio.freq > def_freq - 1.5 and radio.freq < def_freq + 1.5:
+			if timer < max_timer: timer += 1.0 * delta
+		else:
+			if timer > 0: timer -= 3 * delta
 	else:
-		if timer > 0: timer -= 3 * delta
+		if radio.freq > alt_freq - 1.5 and radio.freq < alt_freq + 1.5:
+			if timer < max_timer: timer += 1.0 * delta
+		else:
+			if timer > 0: timer -= 3 * delta
+	
+	#Manejo de la variable alternative_time que indica en que escenario se encuentra actualmente
+	if timer >= max_timer:
+		alternative_time = !alternative_time
+		timer = 0
+	
+	#Cambio de escenografia
+	if alternative_time:
+		alternative_scene.set_process(true)
+		alternative_scene.set_physics_process(true)
+		alternative_scene.set_visible(true)
+		default_scene.set_process(false)
+		default_scene.set_physics_process(false)
+		default_scene.set_visible(false)
+	else:
+		alternative_scene.set_process(false)
+		alternative_scene.set_physics_process(false)
+		alternative_scene.set_visible(false)
+		default_scene.set_process(true)
+		default_scene.set_physics_process(true)
+		default_scene.set_visible(true)
